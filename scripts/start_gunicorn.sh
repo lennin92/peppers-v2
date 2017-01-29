a76c3064d031f75a38d0a9ac01e2a9b6cec441b6
Lennin Hernandez <lennin92@gmail.com>
2017-01-28 23:37:45 -0600

    agregado script para iniciar gunicorn

--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -6,7 +6,7 @@ NAME="peppers"                                      # Name of the application (p
 BIND_SOCKET="0.0.0.0:8787"                          # we will communicte using this socket
 USER="root"                                         # the user to run as
 WORKERS=3                                           # how many worker processes should Gunicorn spawn
-WORKER_TYPE="gevent"                                # worker type (sync, eventel, gevent, tornado) RECOMENDED: gevent
+WORKER_TYPE="sync"                                # worker type (sync, eventel, gevent, tornado) RECOMENDED: gevent
 DJANGO_WSGI_MODULE="peppers.wsgi"                   # WSGI module name
 
 echo "Starting $NAME as `whoami`"
