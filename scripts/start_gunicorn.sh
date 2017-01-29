ac3402cf6857d8c1cdca264b1dc605a86991ade5
Lennin Hernandez <lennin92@gmail.com>
2017-01-28 22:07:16 -0600

    agregado script para iniciar gunicorn

--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -4,10 +4,10 @@ LOG_FILE="/var/log/peppers.log"                     # Path to log file
 LOG_LEVEL="info"                                    # Log level (info, debig, warning, error, critical)
 NAME="peppers"                                      # Name of the application (process name)
 BIND_SOCKET="localhost:8787"                        # we will communicte using this socket
-USER=administrador                                  # the user to run as
+USER="administrador"                                # the user to run as
 WORKERS=3                                           # how many worker processes should Gunicorn spawn
 WORKER_TYPE="gevent"                                # worker type (sync, eventel, gevent, tornado) RECOMENDED: gevent
-DJANGO_WSGI_MODULE=peppers.wsgi                     # WSGI module name
+DJANGO_WSGI_MODULE="peppers.wsgi"                   # WSGI module name
 
 echo "Starting $NAME as `whoami`"
 
